55995fae8d00141d6155ba40133c1a0bcd989f35
Diego <me@diegor.nl>
2022-01-26 14:57:34 +0100

    Create LogPageVisit middleware

---
+++ app/Http/Middleware/LogPageVisit.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Auth;
+use Closure;
+use Illuminate\Http\Request;
+
+class LogPageVisit
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        activity()
+            ->causedBy($request->user())
+            ->withProperties([
+                'user' => $request->user()->name ?? 'Guest',
+                'user_id' => $request->user()->id,
+                'ip' => $request->ip(),
+                'url' => $request->fullUrl(),
+            ])
+            ->event('pageVisit')
+            ->log('Visited page: ' . $request->fullUrl());
+
+        return $next($request);
+    }
+}
